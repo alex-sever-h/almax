e52178af0286ba73b43b5e3f50809e7d60cd4b42
Alexandru-Sever Horin <alex.sever.h@gmail.com>
2018-05-10 18:04:14 +0300

    Add some more

--- init.el
+++ init.el
@@ -450,7 +450,7 @@
  '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
  '(package-selected-packages
    (quote
-    (2048-game magit-filenotify magit-find-file magit-gh-pulls magit rtags avy key-chord guide-key use-package))))
+    (w3 2048-game magit-filenotify magit-find-file magit-gh-pulls magit rtags avy key-chord guide-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
